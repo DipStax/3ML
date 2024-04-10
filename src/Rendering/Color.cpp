3219aad1c5e0f92d83b3333234c708e26f513fe6
DipStax <dipstax.hugo@gmail.com>
2024-04-10 17:32:24 +0200

    feat(Color): alpha application calculation first try

--- src/Rendering/Color.cpp
+++ src/Rendering/Color.cpp
@@ -8,4 +8,34 @@ namespace m3l
             << ", " << static_cast<uint16_t>(_clr.B) << ", " << static_cast<uint16_t>(_clr.A) << " }";
         return _os;
     }
+
+    // Color applyAlpha(const Color &_new, const Color &_old)
+    // {
+    //     Color clr{};
+    //     float old_alpha = _old.A / 255;
+    //     float new_alpha = _new.A / 255;
+    //     float new_ratio = new_alpha / clr.A;
+    //     float old_ratio = old_alpha * (1 - new_alpha) / clr.A;
+
+    //     clr.A = 1 - (1 - old_alpha) * (1 - new_alpha);
+    //     clr.R = _new.R * new_ratio + (_old.R * old_ratio);
+    //     clr.G = _new.G * new_ratio + (_old.G * old_ratio);
+    //     clr.B = _new.B * new_ratio + (_old.B * old_ratio);
+    //     return clr;
+    // }
+
+    uint32_t applyAlpha(uint32_t _new, uint32_t _old)
+    {
+        uint32_t clr{};
+        float old_alpha = CLR_GET_A(_old) / 255;
+        float new_alpha = CLR_GET_A(_new) / 255;
+
+        CLR_SET_A(clr, static_cast<uint8_t>(1 - (1 - old_alpha) * (1 - new_alpha)));
+        float new_ratio = new_alpha / CLR_GET_A(clr);
+        float old_ratio = old_alpha * (1 - new_alpha) / CLR_GET_A(clr);
+        CLR_SET_R(clr, static_cast<uint8_t>(CLR_GET_R(_new) * new_ratio + CLR_GET_R(_old) * old_ratio));
+        CLR_SET_G(clr, static_cast<uint8_t>(CLR_GET_G(_new) * new_ratio + CLR_GET_G(_old) * old_ratio));
+        CLR_SET_B(clr, static_cast<uint8_t>(CLR_GET_B(_new) * new_ratio + CLR_GET_B(_old) * old_ratio));
+        return clr;
+    }
 }
